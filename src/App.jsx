1d6f45aff01c4ab306c1a0041d3f80f13258af29
Avishka <avishkakapuruge@gmail.com>
2023-12-31 12:32:24 +0530

    s6:interaction

--- src/App.jsx
+++ src/App.jsx
@@ -11,10 +11,7 @@ const App = () => {
     <Canvas>
       <directionalLight position={[0, 0, 2]} />
 
-      {/* <Cube positions={[0, 0, 0]} size={[1, 1, 1]} color="green"/> */}
       <Sphere positions={[0, 0, 0]} color="red" size={[1, 30, 30]} />
-      <Torus positions={[2, 0, 0]} color="blue" size={[0.5, 0.1, 30, 30]} />
-      <TorusKnot positions={[-2, 0, 0]} color="orange" size={[0.5, 0.1, 1000, 50]}/>
 
     </Canvas>
   );
--- src/components/Sphere.jsx
+++ src/components/Sphere.jsx
@@ -1,11 +1,29 @@
 /* eslint-disable react/no-unknown-property */
+import { useFrame } from '@react-three/fiber';
 import { PropTypes } from 'prop-types';
+import { useState, useRef } from 'react';
 
 const Sphere = (props) => {
+  const ref = useRef();
+  const [isHovered, setIsHovered] = useState(false);
+  const [isClicked, setIsClicked] = useState(false);
+
+  useFrame((state, delta) => {
+    const speed = isHovered ? 1 : 0.2;
+    ref.current.rotation.y += delta * speed;
+  });
+
   return (
-    <mesh position={props.positions} >
+    <mesh
+      position={props.positions}
+      ref={ref}
+      onPointerEnter={(e) => (e.stopPropagation(), setIsHovered(true))}
+      onPointerLeave={() => setIsHovered(false)}
+      onClick={() => setIsClicked(!isClicked)}
+      scale={isClicked ? 1.5 : 1}
+    >
       <sphereGeometry args={props.size} />
-      <meshStandardMaterial color={props.color} wireframe/>
+      <meshStandardMaterial color={isHovered ? 'green' : props.color} wireframe />
     </mesh>
   );
 };
