1229217975c86c73c52e1b2fe5c2f193a14538a6
Avishka <avishkakapuruge@gmail.com>
2023-12-31 12:59:06 +0530

    s8:helpers

--- src/components/Scene.jsx
+++ src/components/Scene.jsx
@@ -1,12 +1,23 @@
 /* eslint-disable react/no-unknown-property */
-import { OrbitControls } from '@react-three/drei';
+import { OrbitControls, useHelper} from '@react-three/drei';
+import { useRef } from 'react';
+import { DirectionalLightHelper } from 'three';
 import TorusKnot from './TorusKnot';
 
 const Scene = () => {
 
+    const directionalLightRef = useRef()
+
+    useHelper(directionalLightRef, DirectionalLightHelper, 0.5, "white")
+
     return (
         <>
-            <directionalLight position={[0, 0, 2]} />
+            <directionalLight 
+                position={[0, 0, 2]} 
+                intensity={0.5} 
+                ref={directionalLightRef}
+            />
+            <ambientLight intensity={0.5}/>
 
             <TorusKnot positions={[0, 0, 0]} color="red" size={[1, 0.1, 1000, 50]} />
             <OrbitControls enableZoom={false} />
