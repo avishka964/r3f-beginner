e176f9ba236180602f1ae82a85a72a587dbfd444
Avishka <avishkakapuruge@gmail.com>
2023-12-31 13:17:31 +0530

    s9:leva

--- src/components/Scene.jsx
+++ src/components/Scene.jsx
@@ -1,5 +1,6 @@
 /* eslint-disable react/no-unknown-property */
 import { OrbitControls, useHelper} from '@react-three/drei';
+import { useControls } from 'leva';
 import { useRef } from 'react';
 import { DirectionalLightHelper } from 'three';
 import TorusKnot from './TorusKnot';
@@ -8,18 +9,28 @@ const Scene = () => {
 
     const directionalLightRef = useRef()
 
+    const {lightColor, lightIntensity} = useControls({
+        lightColor: "white",
+        lightIntensity: {
+            value: 0.5,
+            min: 0, 
+            max: 5
+        }
+    })
+
     useHelper(directionalLightRef, DirectionalLightHelper, 0.5, "white")
 
     return (
         <>
             <directionalLight 
                 position={[0, 0, 2]} 
-                intensity={0.5} 
+                intensity={lightIntensity} 
                 ref={directionalLightRef}
+                color={lightColor}
             />
             <ambientLight intensity={0.5}/>
 
-            <TorusKnot positions={[0, 0, 0]} color="red" size={[1, 0.1, 1000, 50]} />
+            <TorusKnot positions={[0, 0, 0]} color="red" size={[0.1, 1000, 50]} />
             <OrbitControls enableZoom={false} />
         </>
     );
